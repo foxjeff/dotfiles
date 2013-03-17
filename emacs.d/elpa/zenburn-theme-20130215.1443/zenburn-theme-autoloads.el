d489133942812c0e1188c38a0aa94dd1e4d157a7
Jeff Fox <foxjeff@pobox.com>
2013-03-17 10:01:13 -0500

    changed emacs

---
+++ emacs.d/elpa/zenburn-theme-20130215.1443/zenburn-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (20792 7418))
+;;; Generated autoloads from zenburn-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("zenburn-theme-pkg.el") (20792 7418 569848))
+
+;;;***
+
+(provide 'zenburn-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zenburn-theme-autoloads.el ends here
